3eb381b4fb1da88d1e1df5c8dea8a2cea2aa90c3
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-11 17:00:17 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ async def on_ready():
 
 
 @bot.event
-async def on_raw_reaction_add(reaction, user):
-    print(f"Used reaction: {reaction.emoji}")
-    if reaction.emoji == "✅":
+async def on_raw_reaction_add(emoji, message_id, channel_id, user_id):
+    print(f"Used reaction: {emoji}")
+    if emoji == "✅":
         x = discord.utils.get(bot.get_guild(417154444531204097).roles, name='Verified Member')
         await user.add_roles(x)
         
