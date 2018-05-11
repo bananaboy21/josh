45a4fa00bcfb8e389142d8e8bbf4324e5bc54843
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-10 21:08:10 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,10 +9,9 @@ bot = commands.Bot(command_prefix=">",owner_id=277981712989028353)
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    while True:
-        if reaction.emoji == "✅" and reaction.channel.id == 434763678513102848:
-            x = discord.utils.get(user.guild.roles, name='Verified Member')
-            await user.add_roles(x)
+    if reaction.emoji == "✅":
+        x = discord.utils.get(user.guild.roles, name='Verified Member')
+        await user.add_roles(x)
         
         
         
